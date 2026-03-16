679077e883bc548eb586b37cf47d3a211d97a044
4trastos <info@4trastos.com>
2026-03-16 19:34:24 +0100

    ✅ Level completed!

---
+++ p1/scripts/server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "==>> Instalando dependencias..."
+apt-get update -y
+apt-get install -y curl
+
+SERVER_IP="192.168.56.110"
+
+echo "==>> Instalando K3s en modo server... Añadiendo IP al certificado SSL..."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
+    --tls-san ${SERVER_IP} \
+    --node-ip ${SERVER_IP}" sh -
+
+echo "==>> Esperando a que K3S aranque..."
+sleep 10
+
+echo "==>> Guardando el token para el worker..."
+cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/node-token
+
+echo "==>> Configurando kubectl para el usuario valgrant... (permite usar kubectl directamente sin sudo)"
+mkdir -p /home/vagrant/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+
+echo "==>> Server listo en: ${SERVER_IP}"
---
+++ p1/scripts/worker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "==> Instalando dependencias..."
+apt-get update -y
+apt-get install -y curl
+
+SERVER_IP="192.168.56.110"
+WORKER_IP="192.168.56.111"
+
+echo "==>> Esperando del token del server ... "
+while [ ! -f /vagrant/scripts/node-token ]; do
+    sleep 3
+done
+
+TOKEN=$(cat /vagrant/scripts/node-token)
+
+echo "==>> Instalando K3s en modo agent..."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent \
+    --server https://${SERVER_IP}:6443 \
+    --token ${TOKEN} \
+    --node-ip ${WORKER_IP}" sh -
+
+echo "==>> Worker listo ${WORKER_IP}"
